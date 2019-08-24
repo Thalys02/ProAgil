1635973079f10f42c02c1e0acac948785c7fa941
Thalys MEL <thalys.melicio02@gmail.com>
2019-08-24 02:25:02 -0300

    Inclusão de Layout, Tabela e Filtro

--- ProAgilVSCode.API/Startup.cs
+++ ProAgilVSCode.API/Startup.cs
@@ -30,6 +30,7 @@ namespace ProAgilVSCode.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,8 @@ namespace ProAgilVSCode.API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
